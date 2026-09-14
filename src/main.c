ae71fe4e5c52f5226161e982f3892ab0cdc8a6cb
0rndm_p <277301021+0rndm@users.noreply.github.com>
2026-09-13 22:41:06 -0300

    read file and print content of it

--- src/main.c
+++ src/main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-int main() {
-  printf("Hello, World!\n");
+int main(int argc, char **argv) {
+  argc=argc;
+  char *PathName = argv[1];
+  if (PathName == NULL) {
+    PathName = "main.lua";
+  }
+
+  FILE *InputFile = fopen(PathName, "r");
+
+  int InputSize;
+  fseek(InputFile, 0, SEEK_END);
+  InputSize = ftell(InputFile);
+  fseek(InputFile, 0, SEEK_SET);
+
+  const char *InputBuffer = malloc(InputSize);
+  fread(InputBuffer, 1, InputSize, InputFile);
+
+  printf("%s", InputBuffer)
   return 0;
 }
